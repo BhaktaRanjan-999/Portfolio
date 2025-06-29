0dcfef9392f8ff3aa510c38e901fe6aa0b6836ea
Bhakta Ranjan <bhaktaranjansahu111@gmail.com>
2025-06-29 18:57:59 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,14 @@ AOS.init({
   duration: 1000,
   once: true
 });
+
+const text = "Hi, I'm Bhakta Ranjan Sahu";
+let i = 0;
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 100);
+  }
+}
+window.onload = typeEffect;
